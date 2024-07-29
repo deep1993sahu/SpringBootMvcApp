3beb4da60b1bece4dfe71035abf237352e12aa1d
deepa <deepanjalisahu535@gmail.com>
2024-07-29 15:10:01 +0530

    remove test annotation from test class

--- src/test/java/com/nt/SpringBootMvcProjectWebApplicationMIniProjectApplicationTests.java
+++ src/test/java/com/nt/SpringBootMvcProjectWebApplicationMIniProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.nt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringBootMvcProjectWebApplicationMIniProjectApplicationTests {
 
 	@Test
